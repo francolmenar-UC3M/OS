fadd9f9218aa1905cce50435bb839ca3a967926d
francolmenar <correofalso97@hotmail.es>
2017-04-09 13:26:04 +0200

    Add files via upload

---
+++ process_manager por fin.c
@@ -0,0 +1,33 @@
+/*
+ *
+ * process_manager.c
+ *
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stddef.h>
+#include <pthread.h>
+#include "queue.h"
+#include <semaphore.h>
+
+#define NUM_THREADS 2
+
+
+//Thread function
+void *PrintHello(void *threadid)
+{
+   	long tid;
+   	tid = (long)threadid;
+   	printf("Hello World! It's me, thread #%ld!\n", tid);
+	printf("Thread #%ld ends\n", tid);
+   	pthread_exit(0);
+}
+
+
+int main (int argc, const char * argv[] ){
+	return 0;
+	
+}
