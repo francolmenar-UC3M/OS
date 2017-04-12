bcc242b88816f4112524cbf3df823167a89a389a
francolmenar <correofalso97@hotmail.es>
2017-04-12 11:02:46 +0200

    Me da segmentation faul al pasar a char

--- factory_manager_matrix.c
+++ factory_manager_matrix.c
@@ -12,11 +12,13 @@
 #include <semaphore.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <sys/types.h>
+ #include <sys/wait.h>
 
 /* Method check_number is used to check if the character entered argv is a number or not.
 In that case, returns 0. Otherwise, returns -1. */
 int check_number(char argv){
-	
+
 	/* The number must be in the range of [48,57], this is, [0-9] */
 	if((argv>47) && (argv<58)){
 		return 0;
@@ -28,108 +30,109 @@ int check_number(char argv){
 Returns 0 if correct execution, else, returns -1.
 */
 int main (int argc, const char * argv[] ){
-	
+
 	struct stat *statbuf;    /* Structure to know if the input file is a regular file */
 	int fd,n,size;           /* fd -> file descriptor; n -> number of bytes read from the file; size -> size of the file */
-	int num_rows;            /* Number of processes created by factory_manager, whose parameters are stored in rows */
+	int status,pid, num_rows;            /* Number of processes created by factory_manager, whose parameters are stored in rows */
+
 
 	/* argv[0] --> name of the program
-	   argv[1] --> name of the input file 
+	   argv[1] --> name of the input file
 	   Therefore, number of arguments must be 2 */
 	if(argc!=2){
 		printf("[ERROR][factory_manager] Invalid file\n");
 		exit(-1);
 	}
-	
+
 	/* Allocate memory to statbuf to not get future errors*/
-	statbuf = malloc(sizeof(struct stat));                 
-	
+	statbuf = malloc(sizeof(struct stat));
+
 	/* First, check the stat of the input file */
 	if (stat(argv[1],statbuf)<0){                          /* Get the status to know if it's a regular file */
 		printf("[ERROR][factory_manager] Invalid file\n"); /* Error message */
 		exit(-1);
 	}
-	
+
 	if (S_ISREG(statbuf->st_mode)!=0) {                     /* Check if the input file is a regular file */
-		
+
 		if ((fd=open(argv[1],O_RDONLY,0666))<0) {           /* Open the input file from argv[1] */
-		
+
     	printf("[ERROR][factory_manager] Invalid file\n");  /* Error message */
 		exit(-1);
 		}
-		
+
 		/* We get the size of the input file to store its full content in the buffer buf */
 		if ((size=lseek(fd,0,SEEK_END))<0){                 /* Get the size of the file */
-			
+
 		printf("[ERROR][factory_manager] Invalid file\n");  /* Error message */
 		exit(-1);
 		}
-		
-		
+
+
 		char buf[size];                                     /* buf -> buffer; the size of buffer is the one of the file */
-		
+
 		if ((n=lseek(fd,0,SEEK_SET))<0){                    /* Set the pointer to the beginning of the file again */
-			
+
 		printf("[ERROR][factory_manager] Invalid file\n");  /* Error message */
 		exit(-1);
 		}
-		
+
 		/* Using read, we store the content of the input file in the buffer 'buf' with size 'size', the same of the file */
 		if(read(fd,buf,size)<0) {
-			
+
 			printf("[ERROR][factory_manager] Invalid file\n"); /* Error message */
 			exit(-1);
 		}
- 
+
 		/* Close the file */
 		if (close(fd)<0){
-		
+
 		printf("[ERROR][factory_manager] Invalid file\n"); /* Error message */
 		exit(-1);
 		}
-		
+
 		/* PARSER */
-		/* Now, we start parsing. The objective of the parser is to store the parameters in a matrix in which each row 
+		/* Now, we start parsing. The objective of the parser is to store the parameters in a matrix in which each row
 		contains the information of each process created. Therefore, each row has three columns. The input file is incorrect when:
 		1. The number of processes that must be created are 0
 		2. The content of the file has at least a character different from a number or a space (this include negative numbers)
 		3. More processes are stated than the ones that must be created
-		4. At least one process has not enough parameters 
+		4. At least one process has not enough parameters
 		The content of the input file is retrieved from buf */
-		
+
 		/* Now, declare auxiliary variables*/
 		int i = 0;                      /* i is used for accessing the content of buf via buf[i] */
 		char aux[size];                 /* aux is used for working with strings. size is the maximum length we work with */
-	
+
 		aux[0] = '\0';                  /* At the beginning, aux is empty */
 		char aux2[2];                   /* aux2 is used for working with strings of size 1 */
-		aux2[1] = '\0';                 /* Therefore, we know its last position */      
-		
+		aux2[1] = '\0';                 /* Therefore, we know its last position */
+
 		/* Spaces before the first number are allowed. We skip them using a while. 32 is the number for SPACE is ASCII */
 		while(buf[i]==32){
 			i++;
 		}
 		/* We arrive to the first number, that will be the maximum number of processes created.
-		Check every concatenaded number one by one. Once the char is not a number, skip the while loop. 
+		Check every concatenaded number one by one. Once the char is not a number, skip the while loop.
 		We copy each digit into aux2, to copy them only by one into aux using strcat */
 		while(check_number(buf[i])==0){
 			aux2[0]=buf[i];             /* Copy the digit to aux2 */
 			strcat(aux,aux2);           /* Copy each digit to aux, using strcat */
 			i++;
 		}
-		
+
 		int param[atoi(aux)][4];  /* Declare the resulting matrix as we know its number of rows */
-		
+		size = size -1;
 		/* First thing to do is to check if the number of processes parsed are different from 0.
 		aux can only be a string of digits from 0 to 9. Therefore, atoi only returns 0 if the actual value is 0, not for any error */
 		if (atoi(aux)!=0){
 			/* We declare the auxiliary variables that will be used from now */
 			num_rows = atoi(aux); /* aux will be used for other purposes, so we store the number of rows of the matrix */
-			
+
 			/* j and k are used for indicating the current position of row (j) and colum (k) */
 			int j = 0;
 			int k = 0;
-			
+
 			/* Go over all the content of the buffer, using the counter i -up to size, the length of buf- */
 			while (i<size){
 				/* First we check if the current char is a number or not */
@@ -144,7 +147,7 @@ int main (int argc, const char * argv[] ){
 					/* Correct functioning reaching a digit char.
 					Reset the value of aux --> empty string */
 					memset(aux,0,sizeof aux);
-					
+
 					/* In the same case as in the number of processes, we get the full number digit by digit.
 					The number is stored in aux digit by digit */
 					while(check_number(buf[i])==0){
@@ -169,7 +172,7 @@ int main (int argc, const char * argv[] ){
 						/* If we are not in the last colum, we simply update the column k to k+1 */
 						else{
 							k++;
-						}	
+						}
 					}
 				}
 				/* If the char is not a number, it only can be a SPACE (ASCII 32) to be accepted */
@@ -194,19 +197,64 @@ int main (int argc, const char * argv[] ){
 		/* The number of processes is equal to 0 --> invalid file */
 		else{
 			printf("[ERROR][factory_manager] Invalid file\n"); /* Error message */
-			exit(-1);		
+			exit(-1);
 		}
 		/* PARSER ENDS */
 		/* SYNCHRONIZATION STARTS */
-	
-		for (i = 0; i<num_rows; i++){
-			extern int process_manager(int* param[i]);
+/*
+		int j;
+		int k;
+		printf("%i\n",size);
+		for(j = 0; j < num_rows; j++){
+			for(k = 0; k < 4; k++){
+				printf("%i\t",param[j][k] );
+			}
+			printf("\n" );
 		}
+
+		printf("%i\n",num_rows);*/
+		char **aux_acc;
+		aux_acc = malloc( 4 *sizeof( char* ));
+		int k;
+		printf("joder\n");
+		for (i = 0; i<num_rows; i++){
+			aux_acc[i] = malloc(4);
+				for(k = 0; k < 4; k++){
+					printf("payo %i\n",k);
+					sprintf(aux_acc[k],"%i",param[i][k]);
+					printf("payo despues %i\n",k);
+				}
+ 		pid = fork();
+		switch(pid) {
+
+			case -1:/* Error occurred */
+				perror("Error executing fork");
+				return -1;
+
+			case 0:/* Child process, the one which executes the process */
+			if(execvp("/root/Desktop/2, 2 cuatri/Sistemas/lab3/process_manager.c",aux_acc)<0) { /* Execute the command */
+			perror("Error in method execute");                                /* Error message */
+			return -1;
+			}
+
+			else {
+				/* The parent waits until the children has ended */
+					while (wait(&status) != pid) {
+						/* Check for possible errors */
+						if (status != 0) {
+							perror("Error waiting the children"); /* Error message */
+							return -1;
+						}
+			}
+			printf("Childre arrived\n");
+		}
+	}
+}
 	return 0;
-	}
-	/* The input file is not a regular file --> invalid file */
-	else{
-		printf("[ERROR][factory_manager] Invalid file\n"); /* Error message */
-			exit(-1);
-	}
+}
+/* The input file is not a regular file --> invalid file */
+else{
+	printf("[ERROR][factory_manager] Invalid file\n"); /* Error message */
+		exit(-1);
+}
 }
