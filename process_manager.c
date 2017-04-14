57b4c661f03cc3138df4a920e28b25e15ddc2ed0
francolmenar <correofalso97@hotmail.es>
2017-04-14 12:21:01 +0200

    Update process_manager.c

--- process_manager.c
+++ process_manager.c
@@ -17,8 +17,7 @@
 
 
 //Thread function
-void *PrintHello(void *threadid)
-{
+void *PrintHello(void *threadid){
    	long tid;
    	tid = (long)threadid;
    	printf("Hello World! It's me, thread #%ld!\n", tid);
@@ -29,9 +28,28 @@ void *PrintHello(void *threadid)
 
 int main (int argc, const char * argv[] ){
   printf("Its process_manager\n");
-  for (int i = 1; i < argc;i++){
-    printf("Argument %i:%s\n",i,argv[i]);
-  }
-	return 0;
+  sem_t *sem_process = sem_open(argv[2], O_RDWR);
+  if((sem_process) == SEM_FAILED){
+	  printf("Error");
+	  return -1;
+  }/*  LOOP INFINITO
+  int i;
+    for (i = 0; i < 10; i++) {
+        if (sem_wait(sem_process) < 0) {
+            perror("sem_wait(3) failed on child");
+            continue;
+        }
 
+        printf("PID %ld acquired semaphore\n", (long) getpid());
+
+        if (sem_post(sem_process) < 0) {
+            perror("sem_post(3) error on child");
+        }
+
+        sleep(1);
+    }
+  if (sem_close(sem_process) < 0)
+        perror("sem_close(3) failed");
+*/
+    return 0;
 }
