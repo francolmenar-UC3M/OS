4f4683801e2bcdfd56851976a7df6aa6d1507048
francolmenar <correofalso97@hotmail.es>
2017-04-15 14:57:48 +0200

    Update process_manager.c

--- process_manager.c
+++ process_manager.c
@@ -13,6 +13,9 @@
 #include "queue.h"
 #include <semaphore.h>
 
+
+#define NUM_THREADS 2
+
 //Thread function
 void *PrintHello(void *threadid){
    	long tid;
@@ -36,15 +39,14 @@ int main (int argc, const char * argv[] ){
 		printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);  /* Error message */
   		return -1;
     }
-	
+	sem_post(sem_process);
 	printf("[OK][process_manager] Process_manager with id: %s waiting to produce %s elements.\n",argv[1],argv[4]);
 	
 	if (sem_wait(sem_process)<0) {
         printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
 		return -1;
 	}
-	
-	
-	
+	sem_post(sem_process);
+	printf("[OK][process_manager] Process_manager with id: %s have produced %s elements.\n",argv[1],argv[4]);
     return 0;
 }
