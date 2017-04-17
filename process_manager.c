a7eebc5bc202ec4bb5c49f959d22fcbf8606e0e6
francolmenar <correofalso97@hotmail.es>
2017-04-17 23:32:49 +0200

    Add files via upload

--- process_manager.c
+++ process_manager.c
@@ -16,43 +16,50 @@
 
 //Thread function
 void *PrintHello(void *threadid){
-   	long tid;
-   	tid = (long)threadid;
-   	printf("Hello World! It's me, thread #%ld!\n", tid);
-	printf("Thread #%ld ends\n", tid);
-   	pthread_exit(0);
+        long tid;
+        tid = (long)threadid;
+        printf("Hello World! It's me, thread #%ld!\n", tid);
+        printf("Thread #%ld ends\n", tid);
+        pthread_exit(0);
 }
 
 
 int main (int argc, const char * argv[] ){
-  
-	sem_t *sem_process;
-	
-	if(argc!=5){
-		printf("[ERROR][process_manager] Arguments not valid.\n");
-		return -1;
-	}
-  
-	/* Opening the named semaphore*/
-	if((sem_process = sem_open(argv[2],O_CREAT,0644,0))==SEM_FAILED){
-		printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);  /* Error message */
-  		return -1;
-    }
-	
-	printf("[OK][process_manager] Process_manager with id: %s waiting to produce %s elements.\n",argv[1],argv[4]);
-	
-    if(sem_wait(sem_process) < 0){
-        printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
-		return -1;
-	}
-	
-	printf("[OK][process_manager] Belt with id: %s has been created with a maximum of %s elements.\n",argv[1],argv[4]);
-	
-	if(sem_close(sem_process)<0){
-		printf("[ERROR][factory_manager] Process_manager with id %s has finished with errors.\n", argv[1]); /* Error message*/                        
-		return -1;
-				
-	}	
-	
-	return 0;
+
+        sem_t *sem_process;
+
+        if(argc!=5) {
+                printf("[ERROR][process_manager] Arguments not valid.\n");
+                return -1;
+        }
+
+        /* Opening the named semaphore*/
+        if((sem_process = sem_open(argv[2],O_CREAT,0644,0))==SEM_FAILED) {
+                printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]); /* Error message */
+                return -1;
+        }
+        if(sem_wait(sem_process) < 0) {
+                printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
+                return -1;
+        }
+        printf("[OK][process_manager] Process_manager with id: %s waiting to produce %s elements.\n",argv[1],argv[4]);
+
+        if(sem_wait(sem_process) < 0) {
+                printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
+                return -1;
+        }
+
+        printf("[OK][process_manager] Belt with id: %s has been created with a maximum of %s elements.\n",argv[1],argv[4]);
+
+        if(sem_close(sem_process)<0) {
+                printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
+                return -1;
+
+        }
+        /* Remove the named semaphore referred by semName (free resources). If the result is negative --> error */
+        if(sem_unlink(argv[2])<0) {
+                printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
+                return -1;
+        }
+        return 0;
 }
