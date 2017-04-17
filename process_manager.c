ab88d4a5939ab1aa92767bae10a369b9f0fe4420
Akvamarin <100346091@alumnos.uc3m.es>
2017-04-17 21:10:49 +0200

    Update process_manager.c

--- process_manager.c
+++ process_manager.c
@@ -12,9 +12,7 @@
 #include <pthread.h>
 #include "queue.h"
 #include <semaphore.h>
-
-
-#define NUM_THREADS 2
+#include <signal.h>
 
 //Thread function
 void *PrintHello(void *threadid){
@@ -27,34 +25,34 @@ void *PrintHello(void *threadid){
 
 
 int main (int argc, const char * argv[] ){
-
+  
 	sem_t *sem_process;
-	/* Check the correct input of the parameters */
+	
 	if(argc!=5){
 		printf("[ERROR][process_manager] Arguments not valid.\n");
 		return -1;
 	}
+  
 	/* Opening the named semaphore*/
 	if((sem_process = sem_open(argv[2],O_CREAT,0644,0))==SEM_FAILED){
 		printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);  /* Error message */
   		return -1;
     }
+	
 	printf("[OK][process_manager] Process_manager with id: %s waiting to produce %s elements.\n",argv[1],argv[4]);
-  
-	printf("[OK][process_manager] Process_manager with id: %s have produced %s elements.\n",argv[1],argv[4]);
-    	if(sem_post(sem_process) < 0){
+	
+    if(sem_wait(sem_process) < 0){
         printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
-				return -1;
+		return -1;
 	}
+	
+	printf("[OK][process_manager] Belt with id: %s has been created with a maximum of %s elements.\n",argv[1],argv[4]);
+	
 	if(sem_close(sem_process)<0){
-    printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
-				return -1;
-
-	}
-				/* Remove the named semaphore referred by semName (free resources). If the result is negative --> error */
-	if(sem_unlink(argv[2])<0){
-    printf("[ERROR][process_manager] There was an error executing process_manager with id: %s.\n",argv[1]);
-				return -1;
-	}
+		printf("[ERROR][factory_manager] Process_manager with id %s has finished with errors.\n", argv[1]); /* Error message*/                        
+		return -1;
+				
+	}	
+	
 	return 0;
 }
