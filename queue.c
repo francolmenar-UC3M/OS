bf17bfd92f88231dd874938d6ba3b653de8f21a4
francolmenar <correofalso97@hotmail.es>
2017-04-21 18:52:19 +0200

    Update queue.c

--- queue.c
+++ queue.c
@@ -3,31 +3,11 @@
 #include <stddef.h>
 #include "queue.h" /* https://github.com/stockrt/queue.h/blob/master/sample.c */
 
-struct object
-{
-	int num_edition;  // represents the order of creation inside the belt. It will start from 0 and will be incremented by one for each new element.
-	int id_belt; // represents the id of the belt in which the object is created (this identifier is passed as a parameter to the program).
-	int last; // it will be 0 if it is not the last element to be inserted in the belt and 1 if it is the last one to be inserted.
-};
-
-
-struct queue
-{
-    int capacity;  // maximum number of items in the queue
-    int count;     // number of items in the queue
-    struct object *head;       // pointer to head
-    struct object *tail;       // pointer to tail
-};
-
-
 /*
 	Function that creates the queue and reserves
 	memory for the size specified as parameter.
 */
 int queue_init(int size){
-	struct queue *circular_buffer;
-	circular_buffer = malloc(sizeof(struct queue));
-	circular_buffer -> capacity = size;	
 	return 0;
 }
 
@@ -59,9 +39,5 @@ int queue_full(void){
 
 //To destroy the queue and free the resources
 int queue_destroy(void){
-	return 0;
-}
-int main (int argc, const char * argv[] ){
-	
 	return 0;
 }
