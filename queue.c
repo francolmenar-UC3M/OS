37c2f8e44f0a0a4186a223c4e21e86414bab025a
francolmenar <correofalso97@hotmail.es>
2017-04-24 16:07:57 +0200

    Add files via upload

--- queue.c
+++ queue.c
@@ -22,24 +22,32 @@ int queue_init(int size){
 
 // To Enqueue an element
 int queue_put(struct element* x) {
+	printf("Max capacity: %i\n",max_capacity);
 	if(queue_full() == 1){//Check if the queue is full
 		return -1;//If it is full no inserting is done
 	}
 	else if(queue_empty() == 1){ //If the queue is empty
 		x -> last = 1; //It is the last
 		printf("Last %i\n", x -> last);
+		number_of_elements++; //There is one element more in the queue
+		x -> num_edition = counter;
+		counter++;
+		return 0;
 	}
 	int i;
-	for(i = 0; i < number_of_elements; i++){
+	for(i = 0; i < max_capacity; i++){
+		printf("Iteration %i \n",i);
 		if(circular_buffer[i].last == 1){ //It is the last element in the queue
 			circular_buffer[i].last = 0; //It is no more the last element
 			circular_buffer [(i+1) % max_capacity] = *x; //I insert the element into the queue. It is the possition of the
 			//loop +1 mod max_capacity in order to avoid problems with the last position of the buffer
 		}
 	}
+	printf(" Primero %i\n", number_of_elements);
 	number_of_elements++; //There is one element more in the queue
 	x -> num_edition = counter;
 	counter++;
+	printf(" Segundo %i\n", number_of_elements);
 	return 0;
 }
 
@@ -48,12 +56,13 @@ int queue_put(struct element* x) {
 struct element* queue_get(void) {
 	struct element* aux = malloc(sizeof(struct element));//We allocate the memory needed for the auxiliar element
 	int i;
-	for(i = 0; i < number_of_elements; i++){ //I go through all the queue
+	for(i = 0; i < max_capacity; i++){ //I go through all the queue
 		if(circular_buffer[i].last == 1){ //It is the last element in the queue
 			*aux = circular_buffer [(i+1) % max_capacity];
 		}
 	}
 	number_of_elements--;//There is one element less int the queue
+	printf("Dequeue %i\n", number_of_elements);
 	return aux;
 }
 
@@ -68,7 +77,7 @@ int queue_empty(void){
 }
 
 int queue_full(void){
-	if (number_of_elements != 0) return 1;
+	if (number_of_elements == max_capacity) return 1;
 	else return 0;
 }
 
